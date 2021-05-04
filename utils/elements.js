978e72f337687d22e5f8cde2c7120e7137733871
SGierszewski <stefaniegierszewski@hotmail.com>
2021-05-04 14:25:32 +0200

    ✨Add header, main and footer elements to page layout

---
+++ utils/elements.js
@@ -0,0 +1,10 @@
+export function createElement(tagName, { children, ...props } = {}) {
+  // HTML elements are created; {} = object; ... = rest operator
+  const element = document.createElement(tagName); // HTML elements are created
+  Object.assign(element, props); // element = target, props (properties) = sources which are applied to the target
+  if (children) {
+    // if children = true (optional)
+    element.append(...children); // append... (spread operator) = inserts a set of DOMstring objetcs after the last child of element
+  } // children = array
+  return element;
+}
