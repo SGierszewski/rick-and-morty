456d019ed2cb5a07aba9826988029711106451b8
SGierszewski <stefaniegierszewski@hotmail.com>
2021-05-05 15:25:29 +0200

    ✨Search characters by name

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 import "./style.css";
-import { createElement } from "./utils/elements";
+import { createElement, removeAllChildren } from "./utils/elements";
 import { createCharacterElement } from "./components/character";
 import { getCharacters } from "./utils/api";
 
-getCharacters().then((characters) => {
-  const characterElements = characters.map(createCharacterElement);
-  characterSection.append(...characterElements);
-});
+// getCharacters().then((characters) => {
+//   const characterElements = characters.map(createCharacterElement);
+//   characterSection.append(...characterElements);
+// });
 
 const characterSection = createElement("section", {
   className: "results",
@@ -30,6 +30,22 @@ const mainElement = createElement("main", {
   children: [
     createElement("h2", {
       innerText: "Find your favorite Rick and Morty character",
+    }),
+    createElement("input", {
+      className: "input",
+      placeholder: "Search character",
+      autofocus: true, //input field is focused after each page reload
+      oninput: (event) => {
+        // oninput = input event for every keystroke
+        removeAllChildren(characterSection); //remove children afetr each input to hinder the search results from stacking
+
+        const search = event.target.value; //event.target = self-reference to input field; value = TEXT input
+        getCharacters(search).then((characters) => {
+          //character section is build according to search input
+          const characterElements = characters.map(createCharacterElement);
+          characterSection.append(...characterElements);
+        });
+      },
     }),
     characterSection,
   ],
--- utils/api.js
+++ utils/api.js
@@ -1,5 +1,6 @@
-export async function getCharacters() {
-  return fetch("https://rickandmortyapi.com/api/character").then((response) =>
-    response.json().then((response) => response.results)
-  );
+export function getCharacters(name) {
+  // name to enable search function; add query parameters to URL to enable filtering (?) for e.g. name (query value)
+  return fetch(`https://rickandmortyapi.com/api/character?name=${name}`)
+    .then((response) => response.json())
+    .then((response) => response.results);
 }
